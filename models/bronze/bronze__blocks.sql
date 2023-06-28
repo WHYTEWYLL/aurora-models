cae6b6521537e2f66e014102942891193faf0455
WHYTEWYLL <juanmiblanco1@gmail.com>
2023-06-28 13:54:50 +0200

    add: streamline bronce blocks

---
+++ models/bronze/bronze__blocks.sql
@@ -0,0 +1,24 @@
+{{ config (
+    materialized = 'view',
+    enabled = false
+) }}
+
+SELECT
+    record_id,
+    offset_id,
+    block_id,
+    block_timestamp,
+    network,
+    chain_id,
+    tx_count,
+    header,
+    ingested_at,
+    _inserted_timestamp
+FROM
+    {{ source(
+        'prod',
+        'aurora_blocks'
+    ) }}
+    qualify(ROW_NUMBER() over(PARTITION BY block_id
+ORDER BY
+    ingested_at DESC)) = 1
