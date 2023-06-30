a48b815ea15468024f399058ec9bb034ce751317
WHYTEWYLL <juanmiblanco1@gmail.com>
2023-06-30 02:15:53 +0200

    add silver

--- models/silver/silver__blocks.sql
+++ models/silver/silver__blocks.sql
@@ -9,11 +9,19 @@
 SELECT
     block_number,
     utils.udf_hex_to_int(
-        DATA :result :baseFeePerGas :: STRING
-    ) :: INT AS base_fee_per_gas,
+        DATA :result :timestamp :: STRING
+    ) :: TIMESTAMP AS block_timestamp,
+    -- network
+    -- blockchain
+    ARRAY_SIZE(
+        DATA :result :transactions
+    ) AS tx_count,
     utils.udf_hex_to_int(
         DATA :result :difficulty :: STRING
     ) :: INT AS difficulty,
+    utils.udf_hex_to_int(
+        DATA :result :totalDifficulty :: STRING
+    ) :: INT AS total_difficulty,
     DATA :result :extraData :: STRING AS extra_data,
     utils.udf_hex_to_int(
         DATA :result :gasLimit :: STRING
@@ -21,35 +29,25 @@ SELECT
     utils.udf_hex_to_int(
         DATA :result :gasUsed :: STRING
     ) :: INT AS gas_used,
-    DATA :result :hash :: STRING AS HASH,
-    DATA :result :logsBloom :: STRING AS logs_bloom,
     DATA :result :miner :: STRING AS miner,
     utils.udf_hex_to_int(
         DATA :result :nonce :: STRING
     ) :: INT AS nonce,
+    DATA :result :parentHash :: STRING AS parent_hash,
+    DATA :result :hash :: STRING AS HASH,
+    DATA :result :receiptsRoot :: STRING AS receipts_root,
     utils.udf_hex_to_int(
         DATA :result :number :: STRING
     ) :: INT AS NUMBER,
-    DATA :result :parentHash :: STRING AS parent_hash,
-    DATA :result :receiptsRoot :: STRING AS receipts_root,
     DATA :result :sha3Uncles :: STRING AS sha3_uncles,
     utils.udf_hex_to_int(
         DATA :result :size :: STRING
     ) :: INT AS SIZE,
-    DATA :result :stateRoot :: STRING AS state_root,
-    utils.udf_hex_to_int(
-        DATA :result :timestamp :: STRING
-    ) :: TIMESTAMP AS block_timestamp,
-    utils.udf_hex_to_int(
-        DATA :result :totalDifficulty :: STRING
-    ) :: INT AS total_difficulty,
-    ARRAY_SIZE(
-        DATA :result :transactions
-    ) AS tx_count,
-    DATA :result :transactionsRoot :: STRING AS transactions_root,
     DATA :result :uncles AS uncles,
-    DATA :result :withdrawals AS withdrawals,
-    DATA :result :withdrawalsRoot :: STRING AS withdrawals_root,
+    DATA :result :logsBloom :: STRING AS logs_bloom,
+    DATA :result :stateRoot :: STRING AS state_root,
+    DATA :result :transactionsRoot :: STRING AS transactions_root,
+    _partition_by_block_id,
     _inserted_timestamp
 FROM
 
