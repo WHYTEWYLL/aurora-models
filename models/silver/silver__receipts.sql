621f06df5c960ca5bc2e395c6c1c9b3cfab8d6e8
WHYTEWYLL <juanmiblanco1@gmail.com>
2023-07-12 20:12:30 +0200

    fixL qualify

--- models/silver/silver__receipts.sql
+++ models/silver/silver__receipts.sql
@@ -94,6 +94,6 @@ SELECT
 FROM
     FINAL
 WHERE
-    tx_hash IS NOT NULL qualify(ROW_NUMBER() over (PARTITION BY block_number, POSITION
+    tx_hash IS NOT NULL qualify(ROW_NUMBER() over (PARTITION BY block_number
 ORDER BY
     _inserted_timestamp DESC)) = 1
