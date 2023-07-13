576f52b4117ffa8c9cee15f3c02aad259c02ae7e
WHYTEWYLL <juanmiblanco1@gmail.com>
2023-07-13 22:13:46 +0200

    add: here tag

--- python_scripts/test_alert/dbt_test_alert.py
+++ python_scripts/test_alert/dbt_test_alert.py
@@ -47,7 +47,7 @@ def log_test_result():
 
 def create_message(**kwargs):
     messageBody = {
-        "text": f"Hey{' <@U037AH6DS8P>' if len(kwargs['messages']['fail']) > 0 else ''}, new DBT test results for :{os.environ.get('DATABASE').split('_DEV')[0]}: {os.environ.get('DATABASE')}",
+        "text": f"Hey{' @here' if len(kwargs['messages']['fail']) > 0 else ''}, new DBT test results for :{os.environ.get('DATABASE').split('_DEV')[0]}: {os.environ.get('DATABASE')}",
         "attachments": [
             {
                 "color": kwargs["color"],
