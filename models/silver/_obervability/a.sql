6af9e0432529084e559b8bb3d7f3a8ad0e521ab1
WHYTEWYLL <juanmiblanco1@gmail.com>
2023-08-29 23:32:18 +0200

    obersability models, silver models, migration

---
+++ models/silver/_obervability/a.sql
@@ -0,0 +1,123 @@
+
+
+
+
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'test_timestamp',
+    full_refresh = false,
+    tags = ['observability']
+) }}
+
+WITH summary_stats AS (
+
+    SELECT
+        MIN(block_number) AS min_block,
+        MAX(block_number) AS max_block,
+        MIN(block_timestamp) AS min_block_timestamp,
+        MAX(block_timestamp) AS max_block_timestamp,
+        COUNT(1) AS blocks_tested
+    FROM
+        {{ ref('silver__blocks') }}
+    WHERE
+        block_timestamp <= DATEADD('hour', -12, CURRENT_TIMESTAMP())
+
+{% if is_incremental() %}
+AND (
+    block_number >= (
+        SELECT
+            MIN(block_number)
+        FROM
+            (
+                SELECT
+                    MIN(block_number) AS block_number
+                FROM
+                    {{ ref('silver__blocks') }}
+                WHERE
+                    block_timestamp BETWEEN DATEADD('hour', -96, CURRENT_TIMESTAMP())
+                    AND DATEADD('hour', -95, CURRENT_TIMESTAMP())
+                UNION
+                SELECT
+                    MIN(VALUE) - 1 AS block_number
+                FROM
+                    (
+                        SELECT
+                            blocks_impacted_array
+                        FROM
+                            {{ this }}
+                            qualify ROW_NUMBER() over (
+                                ORDER BY
+                                    test_timestamp DESC
+                            ) = 1
+                    ),
+                    LATERAL FLATTEN(
+                        input => blocks_impacted_array
+                    )
+            )
+    ) {% if var('OBSERV_FULL_TEST') %}
+        OR block_number >= 0
+    {% endif %}
+)
+{% endif %}
+),
+tx_count_block_range AS (
+    SELECT
+        block_number,
+        tx_count as block_tx_count
+    FROM
+        {{ ref('silver__blocks') }}
+    WHERE
+        block_number BETWEEN (
+            SELECT
+                min_block
+            FROM
+                summary_stats
+        )
+        AND (
+            SELECT
+                max_block
+            FROM
+                summary_stats
+        )
+),
+broken_blocks AS (
+    
+    SELECT BLOCK_NUMBER FROM (SELECT
+        block_number
+        COUNT (1) AS tx_count
+    FROM
+        {{ ref("silver__transactions") }} b
+    GROUP BY 1
+        LEFT JOIN tx_per_block
+        t USING (
+            block_number
+        )
+    )
+    WHERE
+        t.blocks_tx_count != b.tx_count
+),
+impacted_blocks AS (
+    SELECT
+        COUNT(1) AS blocks_impacted_count,
+        ARRAY_AGG(block_number) within GROUP (
+            ORDER BY
+                block_number
+        ) AS blocks_impacted_array
+    FROM
+        broken_blocks
+)
+SELECT
+    'transactions' AS test_name,
+    min_block,
+    max_block,
+    min_block_timestamp,
+    max_block_timestamp,
+    blocks_tested,
+    blocks_impacted_count,
+    blocks_impacted_array,
+    CURRENT_TIMESTAMP() AS test_timestamp
+FROM
+    summary_stats
+    JOIN impacted_blocks
+    ON 1 = 1
+
--- models/silver/silver__transactions.sql
+++ models/silver/silver__transactions.sql
@@ -11,7 +11,7 @@ WITH base AS (
 
     SELECT
         block_number,
-        DATA,
+        VALUE AS DATA,
         _inserted_timestamp
     FROM
 
